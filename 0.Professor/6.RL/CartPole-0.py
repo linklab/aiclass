7a6386467d114bdc6bc6c6fa1edc40d3af0023dc
Youn-Hee Han <yh21.han@gmail.com>
2017-04-25 09:00:00 +0900

    …

--- 0.Professor/6.RL/CartPole-0.py
+++ 0.Professor/6.RL/CartPole-0.py
@@ -1,16 +1,23 @@
 import gym
+import os
+
 env = gym.make('CartPole-v0')
 env.reset()
-random_episodes = 0
+episode = 0
 reward_sum = 0
-while random_episodes < 10:
+num_action = 1
+
+while episode < 10:
     env.render()
     action = env.action_space.sample()
     new_state, reward, done, info = env.step(action)
-    print("Action: {0} --> State: {1}, Reward: {2}, Done: {3}, Info: {4}".format(action, new_state, reward, done, info))
+    print("Action {0}: {1} --> State: {2}, Reward: {3}, Done: {4}, Info: {5}".format(num_action, action, new_state, reward, done, info))
     reward_sum += reward
+    num_action += 1
     if done:
-        random_episodes += 1
-        print("Reward for this episode was: {0}".format(reward_sum))
+        print("Total reward for this episode {0} was: {1}".format(episode, reward_sum))
         reward_sum = 0
         env.reset()
+        input("Press Enter to continue...")
+        episode += 1
+        num_action = 1
