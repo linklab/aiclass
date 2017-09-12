19659acde10b787f191ff14b199bd8e227428b03
khg <hy1392@koreatech.ac.kr>
2017-09-12 16:07:36 +0900

    0912

---
+++ 1731036010_HyunGookKim/tf_test.py
@@ -0,0 +1,17 @@
+from tensorflux import graph as tfg
+
+g = tfg.Graph()
+g.initialize()
+
+# Create variables
+a = tfg.Variable(5.0, name="a")
+b = tfg.Variable(1.0, name="b")
+
+# Create placeholder
+x = tfg.Placeholder(name="x")
+
+# Create hidden node y
+y = tfg.Mul(a, x, name="y")
+
+# Create output node z
+z = tfg.Add(y, b, name="z")
