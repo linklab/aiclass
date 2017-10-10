c2add6d42595420494d2c83a75a0004f170afa4c
jupithunder <jubong1992@gmail.com>
2017-10-10 14:06:17 +0900

    update

--- 1731036002_jubongkim/tensorflux/layers.py
+++ 1731036002_jubongkim/tensorflux/layers.py
@@ -9,13 +9,16 @@ import random
 class Affine(tfg.Operation):
     """Returns w * x + b.
     """
-    def __init__(self, w, x, b, name=None):
+    def __init__(self, w, x, b, name=None, graph=None):
         """Construct Affine
 
         Args:
           x: Weight node, y: Input node, b: Bias node
         """
         self.inputs = None
+        graph.add_edge(w, self)
+        graph.add_edge(x, self)
+        graph.add_edge(b, self)
         super().__init__([w, x, b], name)
 
     def forward(self, w_value, x_value, b_value):
@@ -25,8 +28,8 @@ class Affine(tfg.Operation):
           x_value: Weight value, y_value: Input value, b_value: Bias value
         """
         self.inputs = [w_value, x_value, b_value]
-        # return np.matmul(x_value, w_value) + b_value # [Note] Matmax Order
-        return x_value.dot(w_value) + b_value  # [Note] Matmax Order
+        # return np.matmul(x_value, w_value) + b_value # [Note] Matmul Order
+        return x_value.dot(w_value) + b_value  # [Note] Matmul Order
 
     def backward(self):
         pass
@@ -35,14 +38,50 @@ class Affine(tfg.Operation):
         return "Affine: " + self.name
 
 
+class Affine2(tfg.Operation):
+    """Returns w * (x1, x2) + b.
+    """
+    def __init__(self, w, x1, x2, b, name=None, graph=None):
+        """Construct Affine
+
+        Args:
+          x: Weight node, y: Input node, b: Bias node
+        """
+        self.inputs = None
+        graph.add_edge(w, self)
+        graph.add_edge(x1, self)
+        graph.add_edge(x2, self)
+        graph.add_edge(b, self)
+        super().__init__([w, x1, x2, b], name)
+
+    def forward(self, w_value, x1_value, x2_value, b_value):
+        """Compute the output of the add operation
+
+        Args:
+          x_value: Weight value, y_value: Input value, b_value: Bias value
+        """
+        self.inputs = [w_value, x1_value, x2_value, b_value]
+
+        x_input = np.asarray([x1_value, x2_value]).T
+        # return np.matmul(x_value, w_value) + b_value # [Note] Matmul Order
+        return x_input.dot(w_value) + b_value  # [Note] Matmul Order
+
+    def backward(self):
+        pass
+
+    def __str__(self):
+        return "Affine: " + self.name
+
 class ReLU(tfg.Operation):
-    def __init__(self, u, name=None):
+    def __init__(self, u, name=None, graph=None):
         """Construct ReLU
 
         Args:
           u: affine node
         """
         self.inputs = None
+        graph.add_edge(u, self)
+
         self.mask = None
         super().__init__([u], name)
 
@@ -68,13 +107,15 @@ class ReLU(tfg.Operation):
 
 
 class Sigmoid(tfg.Operation):
-    def __init__(self, u, name=None):
+    def __init__(self, u, name=None, graph=None):
         """Construct ReLU
 
         Args:
           u: affine node
         """
         self.inputs = None
+        graph.add_edge(u, self)
+
         self.out = None
         super().__init__([u], name)
 
@@ -91,13 +132,15 @@ class Sigmoid(tfg.Operation):
 
 
 class SquaredError(tfg.Operation):
-    def __init__(self, forward_final_output, target, name=None):
+    def __init__(self, forward_final_output, target, name=None, graph=None):
         """Construct SquaredError
 
         Args:
           output: output node
         """
         self.inputs = None
+        graph.add_edge(forward_final_output, self)
+        graph.add_edge(target, self)
         super().__init__([forward_final_output, target], name)
 
     def forward(self, forward_final_output_value, target_value):
