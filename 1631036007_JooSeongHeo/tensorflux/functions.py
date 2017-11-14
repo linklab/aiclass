8c06787a7a71c0df6ff96f512a7c6452a14c9a8e
HeoJooSeong <zoostar1207@gmail.com>
2017-11-14 15:59:13 +0900

    zoostar

--- 1631036007_JooSeongHeo/tensorflux/functions.py
+++ 1631036007_JooSeongHeo/tensorflux/functions.py
@@ -91,6 +91,80 @@ def accuracy(forward_final_output, target):
     accuracy = np.sum(y == target) / float(forward_final_output.shape[0])
     return accuracy
 
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    input_data : 이미지 데이터
+    filter_h : 필터 높이
+    filter_w : 필터 폭
+    stride : 스트라이드
+    pad : 패드
+
+    Returns
+    -------
+    col : 2차원행렬
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2 * pad - filter_h) / stride + 1
+    out_w = (W + 2 * pad - filter_w) / stride + 1
+
+    assert out_h == int(out_h)
+    out_h = int(out_h)
+
+    assert out_w == int(out_w)
+    out_w = int(out_w)
+
+    img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """
+
+    Parameters
+    ----------
+    col :
+    input_shape : 이미지 데이터 Shape（例：(10, 1, 28, 28)）
+    filter_h
+    filter_w
+    stride
+    pad
+
+    Returns
+    -------
+
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2 * pad - filter_h) / stride + 1
+    out_w = (W + 2 * pad - filter_w) / stride + 1
+
+    assert out_h == int(out_h)
+    out_h = int(out_h)
+
+    assert out_w == int(out_w)
+    out_w = int(out_w)
+
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
 
 if __name__ == "__main__":
     # x1 = get_truncated_normal(shape=(1, 10000), mean=2, sd=1, low=1, upp=10)
@@ -103,28 +177,31 @@ if __name__ == "__main__":
     # ax[2].hist(x3.flatten())
     # plt.show()
 
-    a = np.array([1.0, 2.0, 3.0])
-    print(sigmoid(a, is_numba=True))
-    print()
+    # a = np.array([1.0, 2.0, 3.0])
+    # print(sigmoid(a, is_numba=True))
+    # print()
+    #
+    # b = np.array([1.0, 0.0, 1.0])
+    # c = np.array([0.0, 1.0, 0.0])
+    # print(squared_error(b, c, is_numba=True))
+    # print()
+    #
+    # print(cross_entropy_error(b, c, is_numba=True))
+    # print()
+    #
+    # d = np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]])
+    # print(softmax(d, is_numba=True))
+    # print()
+    #
+    #
+    #
+    # q = np.array([[3.3, 1.2, 9.4], [7.1, 2.2, 3.3], [1.9, 9.2, 2.3]])
+    # t = np.array([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+    # print(accuracy(q, t, is_numba=True))
+    #
+    # q = np.array([[3.3, 1.2, 9.4], [7.1, 2.2, 3.3], [1.9, 9.2, 2.3]])
+    # t = np.array([[1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+    # print(accuracy(q, t, is_numba=True))
 
-    b = np.array([1.0, 0.0, 1.0])
-    c = np.array([0.0, 1.0, 0.0])
-    print(squared_error(b, c, is_numba=True))
-    print()
-
-    print(cross_entropy_error(b, c, is_numba=True))
-    print()
-
-    d = np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]])
-    print(softmax(d, is_numba=True))
-    print()
-
-
-
-    q = np.array([[3.3, 1.2, 9.4], [7.1, 2.2, 3.3], [1.9, 9.2, 2.3]])
-    t = np.array([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
-    print(accuracy(q, t, is_numba=True))
-
-    q = np.array([[3.3, 1.2, 9.4], [7.1, 2.2, 3.3], [1.9, 9.2, 2.3]])
-    t = np.array([[1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
-    print(accuracy(q, t, is_numba=True))
+    x = get_truncated_normal(shape=(100,), mean=0.0, sd=1.0, low=-1.0, upp=1.0)
+    print(x)
